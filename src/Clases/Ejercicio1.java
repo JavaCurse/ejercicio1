d4ac0eab6f2f64914a486a5ce63fe50d95d33c3a
nazareth <naza@gmail.com>
2020-07-21 14:16:15 -0400

    Ejercicio clases metodos

---
+++ src/Clases/Ejercicio1.java
@@ -0,0 +1,13 @@
+package Clases;
+
+public class Ejercicio1 {
+    String mensaja1;
+
+    public static void main(String [] args){
+        Ejercicio1 ejercicio1 = new Ejercicio1();
+        ejercicio1.mensaja1 = "Cada dia es un reto.";
+
+        System.out.println("El mensaje1 es:"+ejercicio1.mensaja1);
+    }
+
+}
